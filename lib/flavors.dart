bf562bfb6a0779c73771f7943cf301a8ea0c620d
junhyeongleeeee <junhyeongleeeee@snaptag.co.kr>
2026-03-20 11:02:25 +0900

    release : v1.0.0

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -31,7 +31,6 @@ class F {
   }
 
   static String get kioskBaseUrl {
-    return 'https://dev-api-spring-kiosk.snaptag.co.kr';
     switch (F.appFlavor) {
       case Flavor.dev:
         return 'https://dev-api-spring-kiosk.snaptag.co.kr';
